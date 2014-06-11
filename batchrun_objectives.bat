654a5c055bb2d185a921585aa231742847e101d9
cgrozemuller <c.g.rozemuller@tudelft.nl>
2014-06-11 17:52:42 +0200

    improved task prioritiy order

--- batchrun_objectives.bat
+++ batchrun_objectives.bat
@@ -8,7 +8,7 @@ set PATH=swifiles/libs;c:\Program Files\java\jre7\bin;c:\Program Files\java\jre6
 set SWI_HOME_DIR=swifiles
 
 :: call mas2g files
-java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0Blockbuster_v2.mas2g" --repeats 100 --timeout 150
+java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0Blockbuster_v2.mas2g" --repeats 100 --timeout 60
 
 ::recursive self calling
 start cmd /c "%~dp0batchrun_objectives.bat"
