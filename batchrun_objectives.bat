8337257e6aa6adb65ec5cf5e9aa944a0c5d037ad
cgrozemuller <c.g.rozemuller@tudelft.nl>
2014-06-24 15:23:43 +0200

    Agent with performance tweaks, and updated BW4Tserver

--- batchrun_objectives.bat
+++ batchrun_objectives.bat
@@ -4,29 +4,30 @@
 
 ::set environment (different per PC)
 cd C:\Users\Chris Rozemuller\GOAL
-set PATH=swifiles/libs;c:\Program Files\java\jre7\bin;c:\Program Files\java\jre6\bin;%PATH%
+::set PATH=swifiles/libs;c:\Program Files\java\jre7\bin;c:\Program Files\java\jre6\bin;%PATH%
+set PATH=swifiles/libs;c:\Program Files\java\jre7\bin
 set SWI_HOME_DIR=swifiles
 
 :: call mas2g files
+java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0Blockbuster_v2.mas2g" --repeats 10000 --timeout 240
 
-::java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0Blockbuster_v2.mas2g" --repeats 100 --timeout 60
+::java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0batch\Blockbuster_v2_10.mas2g" --repeats 2 --timeout 60
+::java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0batch\Blockbuster_v2_1.mas2g" --repeats 2 --timeout 120
+::java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0batch\Blockbuster_v2_3.mas2g" --repeats 2 --timeout 60
+::java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0batch\Blockbuster_v2_5.mas2g" --repeats 2 --timeout 60
+::java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0batch\Blockbuster_v2_7.mas2g" --repeats 2 --timeout 60
+::java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0batch\Blockbuster_v2_9.mas2g" --repeats 2 --timeout 60
 
-java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0batch\Blockbuster_v2_10.mas2g" --repeats 2 --timeout 60
-java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0batch\Blockbuster_v2_1.mas2g" --repeats 2 --timeout 120
-java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0batch\Blockbuster_v2_3.mas2g" --repeats 2 --timeout 60
-java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0batch\Blockbuster_v2_5.mas2g" --repeats 2 --timeout 60
-java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0batch\Blockbuster_v2_7.mas2g" --repeats 2 --timeout 60
-java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0batch\Blockbuster_v2_9.mas2g" --repeats 2 --timeout 60
-
-java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0batch\Blockbuster_v2_10b.mas2g" --repeats 2 --timeout 60
-java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0batch\Blockbuster_v2_1b.mas2g" --repeats 2 --timeout 120
-java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0batch\Blockbuster_v2_3b.mas2g" --repeats 2 --timeout 60
-java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0batch\Blockbuster_v2_5b.mas2g" --repeats 2 --timeout 60
-java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0batch\Blockbuster_v2_7b.mas2g" --repeats 2 --timeout 60
-java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0batch\Blockbuster_v2_9b.mas2g" --repeats 2 --timeout 60
+::java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0batch\Blockbuster_v2_10b.mas2g" --repeats 2 --timeout 60
+::java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0batch\Blockbuster_v2_1b.mas2g" --repeats 2 --timeout 120
+::java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0batch\Blockbuster_v2_3b.mas2g" --repeats 2 --timeout 60
+::java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0batch\Blockbuster_v2_5b.mas2g" --repeats 2 --timeout 60
+::java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0batch\Blockbuster_v2_7b.mas2g" --repeats 2 --timeout 60
+::java -cp goal.jar -Djava.library.path=swifiles/libs goal.tools.Run "%~dp0batch\Blockbuster_v2_9b.mas2g" --repeats 2 --timeout 60
 
 
 
 ::recursive self calling
 start cmd /c "%~dp0batchrun_objectives.bat"
+pause
 exit
